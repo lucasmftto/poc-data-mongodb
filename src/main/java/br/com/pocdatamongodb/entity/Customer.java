6682615d32f1bef0a28036b835ec1b59e8428260
Lucas Favaretto <lucas.favaretto@cielo.com.br>
2021-07-07 10:39:17 -0300

    iniciado o projeto

---
+++ src/main/java/br/com/pocdatamongodb/entity/Customer.java
@@ -0,0 +1,25 @@
+package br.com.pocdatamongodb.entity;
+import org.springframework.data.annotation.Id;
+
+public class Customer {
+
+    @Id
+    public String id;
+
+    public String firstName;
+
+    public String lastName;
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%s, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+
+}
